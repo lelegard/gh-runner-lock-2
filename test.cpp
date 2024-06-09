482ec69b10f5597ba17ffde2f6dc2d10a736a30a
Thierry Lelegard <thierry@lelegard.fr>
2024-06-09 17:04:25 +0200

    Test

--- test.cpp
+++ test.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 int main()
 {
+#if defined(_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR)
+    std::cout << "_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR is defined" << std::flush << std::endl;
+#endif
     std::cout << "1" << std::flush << std::endl;
     std::mutex m;
     std::cout << "2" << std::flush << std::endl;
