b954b1a758afbee2e2f6c2908faae6cdc6698f9e
Thierry Lelegard <thierry@lelegard.fr>
2024-06-09 13:21:23 +0200

    Test

--- setup.ps1
+++ setup.ps1
@@ -5,50 +5,24 @@ function Display-Duration($start)
 }
 
 Write-Output ""
-Write-Output "==== Search all Visual Studio"
-Write-Output ""
-
-$start = Get-Date
-
-Get-ChildItem -Recurse -Depth 1 -Directory "C:\Program Files\Microsoft Visual Studio" |
-    ForEach-Object {$_.FullName}
-
-Display-Duration $start
-
-Write-Output ""
-Write-Output "==== Find MSBuild, fast method"
-Write-Output ""
-
-$start = Get-Date
-
-Get-ChildItem "C:\Program Files\Microsoft Visual Studio\*\*\MSBuild\Current\Bin\MSBuild.exe" |
-    ForEach-Object { Write-Output $_.FullName }
-
-Display-Duration $start
-
-Write-Output ""
-Write-Output "==== Find MSBuild, full search"
-Write-Output ""
-
-$start = Get-Date
-
-$MSRoots = @("C:\Program Files*\MSBuild", "C:\Program Files*\Microsoft Visual Studio")
-Get-ChildItem -Recurse -Path $MSRoots -Include MSBuild.exe -ErrorAction Ignore |
-    ForEach-Object { (Get-Command $_).FileVersionInfo } |
-    Sort-Object -Property FileVersion |
-    ForEach-Object { Write-Output "$($_.FileName) => $($_.FileVersion)" }
-
-Display-Duration $start
-
-Write-Output ""
 Write-Output "==== Search MSVC redistributable installer"
 Write-Output ""
 
 $start = Get-Date
 
-Get-ChildItem -Recurse -Path "C:\Program Files*\Microsoft Visual Studio" -Include "vc*redist*x64.exe" -ErrorAction Ignore |
-    ForEach-Object { (Get-Command $_).FileVersionInfo } |
-    Sort-Object -Property FileVersion  |
-    ForEach-Object { Write-Output "$($_.FileName) => $($_.FileVersion)" }
+$VCRedist64 = Get-ChildItem "C:\Program Files\Microsoft Visual Studio\*\*\VC\Redist\MSVC\*\vc*redist*x64.exe" -ErrorAction Ignore |
+              ForEach-Object { (Get-Command $_).FileVersionInfo } |
+              Sort-Object -Unique -Property FileVersion  |
+              ForEach-Object { $_.FileName} | Select-Object -Last 1
 
 Display-Duration $start
+
+if (-not $VCRedist64) {
+    Write-Output "MSVC Redistributable Libraries 64-bit Installer not found"
+}
+else {
+    Write-Output "Installing $VCRedist64"
+    $start = Get-Date
+    & $VCRedist64 /q /norestart
+    Display-Duration $start
+}
